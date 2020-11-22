56b2ba6555e0615db9a57c480093052be437cf9f
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-11-22 18:20:04 +0100

    added basic css reset/initialization to the app

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,19 @@ export default {
   name: "App",
 };
 </script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "Lato", sans-serif;
+  font-weight: 400;
+  font-size: 1rem;
+  line-height: 1.7;
+  color: #777;
+}
+</style>
