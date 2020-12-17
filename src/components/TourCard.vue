e3358825d1d93f82b6396b6998b0bb36c67e484d
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-12-17 13:25:38 +0000

    implemented the front-back-rotation effect for the tour card component

--- src/components/TourCard.vue
+++ src/components/TourCard.vue
@@ -1,5 +1,50 @@
 <template>
     <div class="tour-card">
-        <h1>card</h1>
+        <div class="tour-card__side tour-card__side--front">
+            <h1>FRONT</h1>
+        </div>
+        <div class="tour-card__side tour-card__side--back">
+            <h1>BACK</h1>
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.tour-card {
+    position: relative;
+    perspective: 1500px;
+    height: 50rem;
+
+    &__side {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        backface-visibility: hidden;
+        height: 50rem;
+        transition: all 1s ease;
+
+        &--front {
+            color: black;
+            background-color: green;
+        }
+
+        &--back {
+            color: black;
+            background-color:indianred;
+            transform: rotateY(180deg);
+        }
+    }
+
+    &:hover > &__ {
+        &side {
+            &--front {
+                transform: rotateY(180deg);
+            }
+            &--back {
+                transform: rotateY(360deg);
+            }
+        }
+    }
+}
+</style>
