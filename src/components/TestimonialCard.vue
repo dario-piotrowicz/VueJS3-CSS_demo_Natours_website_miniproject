037b86d6bce6ad53c499317960a8a36235a17893
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-12-19 20:05:11 +0000

    added circle, title and text to testimonial-card component

--- src/components/TestimonialCard.vue
+++ src/components/TestimonialCard.vue
@@ -1,18 +1,59 @@
 <template>
     <div class="testimonial-card">
-        TESTIMONIAL CARD
+        <figure class="testimonial-card__figure">
+        </figure>
+        <div class="testimonial-card__text-content">
+            <h3 class="testimonial-card__heading">{{ title }}</h3>
+            <p class="testimonial-card__testimonial-text">{{ text }}</p>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>>
 
 <style lang="scss" scoped>
 @import 'src/assets/styles/constants/colors.scss';
 
 .testimonial-card {
     width: 75%;
+    min-height: 27rem;
     margin: 8rem auto;
     box-shadow: 0 3rem 6rem transparentize($color-black, 0.8);
     background-color: $color-white;
     border-radius: 3px;
     padding: 6rem;
+    padding-left: 9rem;
+    font-size: 1.6rem;
+
+    &__figure {
+        $side: 15rem;
+        width: $side;
+        height: $side;
+        float: left;
+        background-color: red;
+        $circle: circle(50% at 50% 50%);
+        shape-outside: $circle;
+        clip-path:  $circle;
+        transform: translate(-3rem);
+    }
+
+    &__heading {
+        margin-bottom: 1rem;
+        font-weight: 700;
+        text-transform: uppercase;
+    }
 }
 </style>
