366b9608e559aaf364bda8741f7ab412e4634b4a
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-12-22 17:13:23 +0000

    using v-for to display all the testimonial cards

--- src/components/sections/Testimonials.vue
+++ src/components/sections/Testimonials.vue
@@ -2,14 +2,13 @@
     <section class="testimonials">
         <div class="text-centering-container">
             <h2 class="section-heading">We make people genuinely happy</h2>
-        <testimonial-card title="I had the best week ever with my family"
-                          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quia voluptate est perspiciatis mollitia rerum doloremque quos dicta dolor quae vel delectus quas, animi quam praesentium blanditiis sed. Itaque quis illum quia reiciendis nam provident. Enim fugiat sit ex dolores?"
-                          :img-src="require('../../assets/images/profiles/mary_smith.jpg')"
-                          img-caption="Mary Smith"> </testimonial-card>
-        <testimonial-card title="WOW! My life is completely different now"
-                          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga recusandae ad inventore consequatur, fugiat molestiae facilis alias maiores! Ullam ea, numquam fuga libero! Enim, numquam. Similique corrupti vitae ratione. Sint cumque enim pariatur quis totam id."
-                          :img-src="require('../../assets/images/profiles/jack_wilson.jpg')"
-                          img-caption="Jack Wilson"></testimonial-card>
+                <testimonial-card v-for="card of testimonialCards"
+                                  :key="card.id"
+                                  :title="card.title"
+                                  :text="card.text"
+                                  :img-src="card.imgSrc"
+                                  :img-caption="card.imgCaption">
+                </testimonial-card>
         <text-link-button href="#" text="Read all"></text-link-button>
         </div>
     </section>
@@ -22,6 +21,26 @@ export default {
     components: {
         "testimonial-card": TestimonialCard,
         "text-link-button": TextLinkButton
+    },
+    data: function(){
+        return {
+            testimonialCards: [
+                {
+                    id:'mary_smith_0',
+                    title: "I had the best week ever with my family",
+                    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quia voluptate est perspiciatis mollitia rerum doloremque quos dicta dolor quae vel delectus quas, animi quam praesentium blanditiis sed. Itaque quis illum quia reiciendis nam provident. Enim fugiat sit ex dolores?",
+                    imgSrc: require('../../assets/images/profiles/mary_smith.jpg'),
+                    imgCaption: "Mary Smith"
+                },
+                {
+                    id:'jack_wilson_0',
+                    title: "WOW! My life is completely different now",
+                    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga recusandae ad inventore consequatur, fugiat molestiae facilis alias maiores! Ullam ea, numquam fuga libero! Enim, numquam. Similique corrupti vitae ratione. Sint cumque enim pariatur quis totam id.",
+                    imgSrc: require('../../assets/images/profiles/jack_wilson.jpg'),
+                    imgCaption: "Jack Wilson"
+                }
+            ]
+        }
     }
 }
 </script>
