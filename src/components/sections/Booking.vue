ec66bc72dd50d22fe4a4b40683338267876aa34c
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-12-24 12:33:30 +0000

    setting the background white section for the booking section's content using the solid gradient color technique

--- src/components/sections/Booking.vue
+++ src/components/sections/Booking.vue
@@ -22,7 +22,14 @@ export default {
     &__content {
         height: 50rem;
 
-        background-image: url("../../assets/images/booking-bg.jpg");
+        background-image: linear-gradient(
+                105deg,
+                transparentize($color-white, 0.18) 0%,
+                transparentize($color-white, 0.18) 49.9%,
+                transparent 50%
+            ),
+            url("../../assets/images/booking-bg.jpg"
+        );
         background-size: cover;
         border-radius: 3px;
         box-shadow: 0 1.5rem 4rem transparentize($color-black, 0.6);
