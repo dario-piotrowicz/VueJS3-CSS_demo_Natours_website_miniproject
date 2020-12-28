85556ecfeafd017d9dccae638ac253aa21947e74
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-12-28 12:45:05 +0000

    set up the navigation component's markup

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,29 @@
 <template>
     <div class="navigation">
+        <input type="checkbox" class="navigation__checkbox" id="navigation-hidden-checkbox">
+        <label for="navigation-hidden-checkbox" class="navigation__button">
+            MENU
+        </label>
+        <div class="navigation__background"></div>
+        <nav class="navigation__menu">
+            <ul class="navigation__list">
+                <li class="navigation__list-item">
+                    <a href="#" class="navigation__link">About Natours</a>
+                </li>
+                <li class="navigation__list-item">
+                        <a href="#" class="navigation__link">Your benefits</a>
+                </li>
+                <li class="navigation__list-item">
+                        <a href="#" class="navigation__link">Popular tours</a>
+                </li>
+                <li class="navigation__list-item">
+                        <a href="#" class="navigation__link">Stories</a>
+                </li>
+                <li class="navigation__list-item">
+                        <a href="#" class="navigation__link">Book now</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
